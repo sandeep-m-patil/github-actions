50f4abd68af6a6b18c0262512312140dfcacbc93
sandeep <sandeeppatil10705@gmail.com>
2025-03-23 11:29:40 +0530

    added main.c file

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+// Function to be tested
+int add(int a, int b) {
+    return a + b;
+}
+
+int main() {
+    printf("Program started...\n");
+
+    // The test cases will be appended here by GitHub Actions
+    return 0;
+}
